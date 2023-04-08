6c11b9fb4f1dc184c87f3f7046111f63c50e7629
Vahid Farid <vahidfarid@gmail.com>
2023-04-08 19:20:52 +0330

    Update collect-json.py

--- collect-json.py
+++ collect-json.py
@@ -3,12 +3,6 @@ import json
 import dns
 import dns.resolver
 
-class IPInfo:
-    def __init__(self, ip, operator, provider, created_at):
-        self.ip = ip
-        self.operator = operator
-        self.provider = provider
-        self.created_at = created_at
 
 result = {
   "last_update": time.strftime("%Y-%m-%d %H:%M"),
@@ -16,12 +10,59 @@ result = {
   "ipv6": []
 }
 
-provider = "cloudflare.com"
-operator = "TEST"
+providers = {
+    "mci.ircf.space": "MCI",
+    "mcix.ircf.space": "MCI",
+    "mtn.ircf.space": "MTN",
+    "mtnx.ircf.space": "MTN",
+    "mkh.ircf.space": "MKH",
+    "rtl.ircf.space": "RTL",
+    "hwb.ircf.space": "HWB",
+    "ast.ircf.space": "AST",
+    "sht.ircf.space": "SHT",
+    "prs.ircf.space": "PRS",
+    "mbt.ircf.space": "MBT",
+    "ask.ircf.space": "ASK",
+    "rsp.ircf.space": "RSP",
+    "afn.ircf.space": "AFN",
+    "ztl.ircf.space": "ZTL",
+    "psm.ircf.space": "PSM",
+    "arx.ircf.space": "ARX",
+    "smt.ircf.space": "SMT",
+    "fnv.ircf.space": "FNV",
+    "dbn.ircf.space": "DBN",
+    "apt.ircf.space": "APT",
+    "mci.amiajoketoyou.lol": "MCI",
+    "mtn.amiajoketoyou.lol": "MTN",
+}
 
-ipv4_result = dns.resolver.resolve(provider, "A")
+for provider in providers:
+    # IPv4
+    try:
+        ipv4_result = dns.resolver.resolve(provider, "A")
+        for ipv4 in ipv4_result:
+            result["ipv4"].append({
+                "ip": ipv4.to_text(),
+                "operator": providers[provider],
+                "provider": provider,
+                "created_at": int(time.time())
+            })
+    except:
+        # Ignore
+        ignored = True
 
-for ipv4 in ipv4_result:
-    result["ipv4"].append(IPInfo(ipv4.to_text(), operator, provider, int(time.time())))
+    # IPv6
+    try:
+        ipv6_result = dns.resolver.resolve(provider, "AAAA")
+        for ipv6 in ipv6_result:
+            result["ipv6"].append({
+                "ip": ipv6.to_text(),
+                "operator": providers[provider],
+                "provider": provider,
+                "created_at": int(time.time())
+            })
+    except:
+        # Ignore
+        ignored = True
 
 print(json.dumps(result, indent=4))

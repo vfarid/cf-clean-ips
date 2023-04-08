fc8e80925db63460a6fe0391046640377875ce43
Vahid Farid <vahidfarid@gmail.com>
2023-04-08 18:30:45 +0330

    Update collect-json.py

--- collect-json.py
+++ collect-json.py
@@ -1,10 +1,18 @@
 import time
 import json
+import dns
+import dns.resolver
 
 result = {
   "last_update": time.strftime("%Y-%m-%d %H:%M"),
   "ipv4": {},
   "ipv6": {}
 }
+
+provider = "cloudflare.com"
+ipv4_result = dns.resolver.resolve(provider, "A")
+
+for ipv4 in ipv4_result:
+    result.ipv4.append({"ip": ipv4.to_text(), "provider": provider})
 
 print(json.dumps(result, indent=4))

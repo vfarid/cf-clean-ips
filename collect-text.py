76329c7a75f65b56fbd13f9ac658e2b4b4ae7ddf
Vahid Farid <farid@aradsolution.com>
2023-04-10 01:15:33 +0330

    Update collect-text.py

--- collect-text.py
+++ collect-text.py
@@ -3,32 +3,7 @@ import json
 import dns
 import dns.resolver
 
-
-providers = {
-    "mci.ircf.space": "MCI",
-    "mcix.ircf.space": "MCI",
-    "mtn.ircf.space": "MTN",
-    "mtnx.ircf.space": "MTN",
-    "mkh.ircf.space": "MKH",
-    "rtl.ircf.space": "RTL",
-    "hwb.ircf.space": "HWB",
-    "ast.ircf.space": "AST",
-    "sht.ircf.space": "SHT",
-    "prs.ircf.space": "PRS",
-    "mbt.ircf.space": "MBT",
-    "ask.ircf.space": "ASK",
-    "rsp.ircf.space": "RSP",
-    "afn.ircf.space": "AFN",
-    "ztl.ircf.space": "ZTL",
-    "psm.ircf.space": "PSM",
-    "arx.ircf.space": "ARX",
-    "smt.ircf.space": "SMT",
-    "fnv.ircf.space": "FNV",
-    "dbn.ircf.space": "DBN",
-    "apt.ircf.space": "APT",
-    "mci.amiajoketoyou.lol": "MCI",
-    "mtn.amiajoketoyou.lol": "MTN",
-}
+providers = json.load(open('providers.json'))
 
 print("Last Update: " + time.strftime("%Y-%m-%d %H:%M"))
 
@@ -38,7 +13,7 @@ for provider in providers:
     try:
         ipv4_result = dns.resolver.resolve(provider, "A")
         for ipv4 in ipv4_result:
-            print(f"  - {ipv4.to_text():15s}    {providers[provider]:5s}    {provider}    {int(time.time())}")
+            print(f"  - {ipv4.to_text():15s}    {providers[provider]:5s}    {'.'.join(provider.split('.')[1:])}    {int(time.time())}")
     except:
         # Ignore
         ignored = True
@@ -50,7 +25,7 @@ for provider in providers:
     try:
         ipv6_result = dns.resolver.resolve(provider, "AAAA")
         for ipv6 in ipv6_result:
-            print(f"  - {ipv4.to_text():32s}    {providers[provider]:5s}    {provider}    {int(time.time())}")
+            print(f"  - {ipv4.to_text():32s}    {providers[provider]:5s}    {'.'.join(provider.split('.')[1:])}    {int(time.time())}")
     except:
         # Ignore
         ignored = True
